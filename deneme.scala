6bae2c7a4172d735859494d653653eaaf6647303
ozgur b <ezopezop@gmail.com>
2012-12-05 22:51:25 +0200

    ikinci degisim

--- deneme.scala
+++ deneme.scala
@@ -2,6 +2,6 @@
 
 abstract class Element {
   def contents: Array[String]
-  def height: Int = contents.length
-  def with: Int = if(contents.length == 0) 0 else contents(0).length  
+  val height: Int = contents.length
+  val with: Int = if(contents.length == 0) 0 else contents(0).length  
 }
