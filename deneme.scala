1a749630a2b1d04427740b7d7a74bd1830b78c98
ozgur b <ezopezop@gmail.com>
2012-12-05 23:05:16 +0200

    dorduncu surum

--- deneme.scala
+++ deneme.scala
@@ -5,3 +5,7 @@ abstract class Element {
   val height: Int = contents.length
   val with: Int = if(contents.length == 0) 0 else contents(0).length  
 }
+
+Class ArrayElement(conts: Array[Sting]) extends Element {
+	val contents: Array[String] = conts
+}
